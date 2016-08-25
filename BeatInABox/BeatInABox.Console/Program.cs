e412871557e6aea680b610a508bba5d20e7ad4b8
Scott Metoyer <smetoyer@libraries.ucr.edu>
2016-08-25 13:34:48 -0700

    Stubbed out core logic

--- BeatInABox/BeatInABox.Console/Program.cs
+++ BeatInABox/BeatInABox.Console/Program.cs
@@ -10,6 +10,21 @@ namespace BeatInABox.Console
     {
         static void Main(string[] args)
         {
+            List<Clip> allClips = new List<Clip>();
+            var files = GetFiles(searchPattern);
+
+            foreach (var f in files)
+            {
+                List<Clip> clips = ClipSlicer.Slice(file, rules);
+                allClips.AddRange(clips);
+            }
+
+            Sequence sequence = Sequence.Create(sequenceOptions);
+
+            var wave = sequence.Render(allClips, renderOptions);
+            SaveToDisk(wave, filename);
+
+            System.Console.WriteLine("Sequence saved to disk.");
             System.Console.WriteLine("Press any key to continue...");
             System.Console.Read();
         }
