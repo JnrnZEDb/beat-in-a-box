e7f31d055596f8bfb34ee6e1ad45c8061813612e
Scott Metoyer <scott.metoyer@gmail.com>
2016-08-27 07:50:50 -0700

    Added stubbed classes

--- BeatInABox/BeatInABox.Console/Program.cs
+++ BeatInABox/BeatInABox.Console/Program.cs
@@ -13,11 +13,15 @@ namespace BeatInABox.Console
             List<Clip> allClips = new List<Clip>();
             var files = GetFiles(searchPattern);
 
+            System.Console.WriteLine("Loaded " + files.Length + " files from disk.");
+
             foreach (var f in files)
             {
                 List<Clip> clips = ClipSlicer.Slice(file, rules);
                 allClips.AddRange(clips);
             }
+
+            System.Console.WriteLine("Created " + allClips.Length + " clips.");
 
             Sequence sequence = Sequence.Create(sequenceOptions);
 
